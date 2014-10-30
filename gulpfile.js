4069068ba2f4dd9f9bce62501c32d274d6e64a88
Yacine Rezgui <rezgui.y@gmail.com>
2014-10-30 22:55:26 +0000

    Add Gulpfile for build system

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp        = require('gulp');
+var browserify  = require('gulp-browserify');
+var concat      = require('gulp-concat');
+var sass        = require('gulp-ruby-sass');
+
+gulp.task('script', function() {
+  gulp.src('src/js/main.js')
+      .pipe(browserify({transform: 'reactify'}))
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('style', function () {
+  return gulp.src('src/css/app.scss')
+              .pipe(sass({sourcemap: true, sourcemapPath: '../scss'}))
+              .on('error', function (err) { console.log(err.message); })
+              .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('template', function() {
+  gulp.src('src/index.html')
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default',['script', 'style', 'template']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.html', ['template']);
+  gulp.watch('src/**/*.scss', ['style']);
+  gulp.watch('src/**/*.js',   ['script']);
+});
