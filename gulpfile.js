dc250d999b215dda31bca464716cd8d244a76ae5
Yacine Rezgui <rezgui.y@gmail.com>
2014-10-31 00:54:55 +0000

    Update location of source folders Remove source map parameter (deprecated, enabled by default)

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ var concat      = require('gulp-concat');
 var sass        = require('gulp-ruby-sass');
 
 gulp.task('script', function() {
-  gulp.src('src/js/main.js')
+  gulp.src('src/scripts/main.js')
       .pipe(browserify({transform: 'reactify'}))
       .pipe(concat('main.js'))
       .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('style', function () {
-  return gulp.src('src/css/app.scss')
-              .pipe(sass({sourcemap: true, sourcemapPath: '../scss'}))
+  return gulp.src('src/styles/main.scss')
+              .pipe(sass())
               .on('error', function (err) { console.log(err.message); })
               .pipe(gulp.dest('dist/css'));
 });
