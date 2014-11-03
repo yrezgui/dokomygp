5ad0cdb1259352d9ae260212db43c456e2ba7894
Yacine Rezgui <rezgui.y@gmail.com>
2014-11-03 10:09:54 +0000

    Add App constants

---
+++ src/scripts/constants/app-constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  API_URL: 'https://api.import.io/store/data/3cf27c3b-c9e2-45c8-8a97-c4fc559ed74f/_query?_apikey=Xk6Emf7P1fSYy8lvBrvM0ygZdrQZytV5AAR4ykx5R%2FlwmgvPBYTH7XRwObZESjTduH6C0fsqr9N5y2LC9btu6g%3D%3D&_user=c5b82e89-e0b8-41f9-bdd5-f13f5925b0bb&input/webpage/url=',
+  NHS_URL: 'http://www.nhs.uk/Service-Search/GP/England/Results/4/',
+  EXTRA_NHS_PARAMS: '/4/0?distance=25&ResultsOnPageValue=30',
+};
