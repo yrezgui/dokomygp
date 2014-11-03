7e9b9e75c7be281014af2119f450fd34d7f40209
Yacine Rezgui <rezgui.y@gmail.com>
2014-11-03 13:41:27 +0000

    Add StatusMessage component

---
+++ src/scripts/components/status-message.js
@@ -0,0 +1,35 @@
+var React     = require('react');
+var AppStatus = require('../constants/app-status');
+
+var StatusMessage = React.createClass({
+  render: function() {
+    
+    var messageClass = 'vertical-center text-center ';
+    var messageText  = '';
+
+    switch(this.props.status) {
+
+      case AppStatus.DONE:
+        messageClass += 'hide';
+        break;
+
+      case AppStatus.LOADING:
+        messageText = 'Loading...';
+        break;
+
+      case AppStatus.GEOLOCATION_FAIL:
+        messageText = 'Your geolocation position isn\'t available now. Please try later.';
+        break;
+
+      case AppStatus.API_FAIL:
+        messageText = 'We couldn\'t reach the server. Please try later.';
+        break;
+    }
+
+    return (
+      <h2 className={messageClass}>{messageText}</h2>
+    );
+  }
+});
+
+module.exports = StatusMessage;
